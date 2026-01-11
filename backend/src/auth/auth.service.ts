46a7669a1ca005dc8fab3255443ad76847535f0e
nayabrasoolsb <nrasool030@gmail.com>
2026-01-11 22:50:06 +0530

    Nayab | change: login service error message

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
@@ -8,27 +8,41 @@ export class AuthService {
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
+  ) { }
 
   async login(username: string, password: string) {
-    const user = await this.usersService.findByUsername(username);
-    if (!user) throw new UnauthorizedException();
+    try {
 
-    const pepper = process.env.PASSWORD_PEPPER!;
-    const isValid = await bcrypt.compare(
-      password + pepper,
-      user?.password,
-    );
+      const user = await this.usersService.findByUsername(username);
+      if (!user) {
+        throw new UnauthorizedException("User not found");
+      }
+      const pepper = process.env.PASSWORD_PEPPER!;
+      const isValid = await bcrypt.compare(
+        password + pepper,
+        user?.password,
+      );
 
-    if (!isValid) throw new UnauthorizedException();
+      if (!isValid) {
+        throw new UnauthorizedException("Incorrect password");
+      }
 
-    const payload = {
-      sub: user?.id,
-      username: user?.username,
-    };
+      const payload = {
+        sub: user?.id,
+        username: user?.username,
+      };
 
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      if (error instanceof UnauthorizedException) {
+        throw error;
+      }
+
+      throw new InternalServerErrorException(
+        "Error occurred while logging in"
+      );
+    }
   }
 }
