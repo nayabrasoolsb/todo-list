b1fe0dc79d4172685fbfca53fc2a981e046179af
nayabrasoolsb <nrasool030@gmail.com>
2026-01-11 23:31:06 +0530

    Nayab | change: fetch todos by page

--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { TodoStatus } from '@prisma/client';
 import { CreateTodoDto } from './dto/create-todo.dto';
@@ -6,7 +6,7 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 
 @Injectable()
 export class TodoService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   createTodo(dto: CreateTodoDto, username: string) {
     return this.prisma.todo.create({
@@ -18,10 +18,33 @@ export class TodoService {
     });
   }
 
-  findAllTodos() {
-    return this.prisma.todo.findMany({
-      orderBy: { createdAt: 'desc' },
-    });
+  async findAllTodos(page: number, limit: number) {
+    const skip = (page - 1) * limit;
+
+    try {
+      const [data, total] = await this.prisma.$transaction([
+        this.prisma.todo.findMany({
+          skip,
+          take: limit,
+          orderBy: { createdAt: 'desc' },
+        }),
+        this.prisma.todo.count(),
+      ]);
+
+      return {
+        data,
+        meta: {
+          total,
+          page,
+          limit,
+          totalPages: Math.ceil(total / limit),
+        },
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to fetch todos',
+      );
+    }
   }
 
   updateTodoStatus(id: number, status: TodoStatus) {
